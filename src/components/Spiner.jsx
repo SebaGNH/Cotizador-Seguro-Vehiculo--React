2c6615a3dde51299a89c02c26492c8178fbeb6f6
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-08-31 11:09:58 -0300

    se centró sppinner

--- src/components/Spiner.jsx
+++ src/components/Spiner.jsx
@@ -3,13 +3,15 @@ import '../Styles/Spinner.css';
 
 const Spinner = () => {
   return (
-    <div className="sk-chase">
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
+    <div className="contenedor-spinner">
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
     </div>
   );
 }
